0d17cc2a75cdb1efdc3b24eba615cf7875ca0e43
amr alaa <amraalaa22@gmail.com>
2023-05-31 16:33:40 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,15 @@
+// header
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+})
+// menu --x--
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
